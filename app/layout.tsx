a7c7197d9921510ba514a72bca46d228ebf1a4b6
Pritam Das Adhikari <pritamdasadhikari8917@gmail.com>
2026-03-13 03:23:37 +0530

    feat: add main components for portfolio including Navbar, Footer, About, Skills, Experience, Projects, and Contact

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,13 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { Navbar } from "./navbar/page";
-import About from "./about/page";
-import Skills from "./skills/page";
-import Experience from "./experience/page";
-import Projects from "./projects/page";
-import Contact from "./contact/page";
-import Footer from "./footer/page";
+import {Navbar} from "@/components/navbar";
+import Footer from "@/components/footer";
+import About from "@/components/about";
+import Skills from "@/components/skills";
+import Experience from "@/components/experience";
+import Projects from "@/components/projects";
+import Contact from "@/components/contact";
 
 import SmoothScroll from "@/components/SmoothScroll";
 
@@ -38,9 +38,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SmoothScroll />
-
         <Navbar />
-
         <main>
           {children}
           <About />
